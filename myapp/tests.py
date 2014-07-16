be2115bad32154f5b793be8c36a6b290c8ffe105
Evgeniy <shmatovzhenya@yandex.ru>
2014-07-16 18:31:08 +0400

    Дописал тесты на действующий функционал (не работающий edit не считается)

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,58 @@
+import json
+
 from django.test import TestCase
+from django.test.client import Client
 
 # Create your tests here.
+from .models import tables, sources
+
+
+class MainPageTest(TestCase):
+    fixtures = ['users.json', 'rooms.json']
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_main_page_available(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_tablenames(self):
+        response = self.client.get('/tablenames')
+        self.assertEqual(response.status_code, 200)
+        names = json.dumps([sources[item]['title'] for item in sources],
+                           ensure_ascii=False)
+        self.assertEqual(str(response.content, 'utf-8'), names)
+
+    def test_table_columns(self):
+        for table in tables:
+            response = self.client.get('/%s' % sources[table]['title'])
+
+            self.assertEqual(response.status_code, 200)
+
+            kwargs = [item['id'] for item in sources[table]['fields']]
+            data = tables[table].objects.values(*kwargs)
+            for i, item in enumerate(
+                    json.loads(str(response.content, 'utf-8'))['data']):
+                for key in item:
+                    self.assertEqual(sorted(item), sorted(
+                                     [str(data[i][key]) for key in data[i]]))
+
+        response = self.client.get('/sfsdfdsfsd')
+        self.assertEqual(response.status_code, 404)
+
+    def test_add_value(self):
+        convert = {'int': 1, 'char': 'aaa', 'date': 'Jul 03 2014'}
+        for table in tables:
+            data = {}
+            for item in sources[table]['fields']:
+                data[item['id']] = convert[item['type']]
+
+            response = self.client.post(
+                '/add/%s' % sources[table]['title'], data)
+            self.assertEqual(response.status_code, 200)
+
+            data = {}
+            response = self.client.post(
+                '/add/%s' % sources[table]['title'], {})
+            self.assertEqual(response.status_code, 500)
--- zadanie/views.py
+++ zadanie/views.py
@@ -2,7 +2,7 @@ import json
 
 from django.http import HttpResponse, HttpResponseNotFound
 from django.http import HttpResponseServerError
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.views.decorators.csrf import csrf_exempt
 
 from myapp.models import tables, sources
@@ -10,7 +10,7 @@ from myapp.forms import myforms
 
 
 def index(request):
-    return render(request, 'base.html')
+    return render_to_response('base.html')
 
 
 def tablenames(request):
@@ -22,44 +22,49 @@ def tablenames(request):
 def table_columns(request, table):
     convert = {'int': 'number', 'char': 'text', 'date': 'date',
                'number': 'number', 'text': 'text'}
-    try:
-        for key in sources:
-            if sources[key]['title'] == table:
-                src = sources[key]['fields']
+    for key in sources:
+        if sources[key]['title'] == table:
+            src = sources[key]['fields']
 
-                result = {}
-                result['columns'] = src
-                for column in result['columns']:
-                    column['type'] = convert[column['type']]
-                    column['result'] = ''
-                kwargs = [item['id'] for item in sources[key]['fields']]
-                result['data'] = list(tables[key].objects.values(*kwargs)) or list()
-                print(result['data'])
-                for item in result['data']:
-                    for key in item:
-                        if str(item[key]).find('datetime'):
-                            item[key] = str(item[key])
-                js = json.dumps(result, ensure_ascii=False)
-                return HttpResponse(js, content_type='application/json')
-        return HttpResponseNotFound()
-    except Exception as e:
-        print(e)
-        return HttpResponse()
+            result = {}
+            result['columns'] = src
+            for column in result['columns']:
+                column['type'] = convert[column['type']]
+                column['result'] = ''
+            kwargs = [item['id'] for item in sources[key]['fields']]
+            result['data'] = list(tables[key].objects.values(*kwargs)) or list()
+
+            tmp = []
+            for item in result['data']:
+                for key in item:
+                    if str(item[key]).find('datetime'):
+                        item[key] = str(item[key])
+                columns = [item['id'] for item in result['columns']]
+                tmp.append([item[column] for column in columns]) #!!
+            result['data'] = tmp
+
+            js = json.dumps(result, ensure_ascii=False)
+            return HttpResponse(js, content_type='application/json')
+    return HttpResponseNotFound()
 
 
 @csrf_exempt
 def add_value(request, table):
+    for key in sources:
+        if sources[key]['title'] == table:
+            form = myforms['%sForm' % key](request.POST)
+            if form.is_valid():
+                cd = form.cleaned_data
+                tables[key].objects.create(**cd)
+                return HttpResponse()
+            else:
+                print('invalid')
+                print(form.errors)
+                return HttpResponseServerError()
+
+@csrf_exempt
+def edit_value(request, tables):
     try:
-        for key in sources:
-            if sources[key]['title'] == table:
-                form = myforms['%sForm' % key](request.POST)
-                if form.is_valid():
-                    cd = form.cleaned_data
-                    tables[key].objects.create(**cd)
-                else:
-                    print(request.POST)
-                    print('invalid')
-                    print(form.errors)
         return HttpResponse()
     except Exception as e:
         print(e)
